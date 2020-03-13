d433c5b6ac64b810d359ff1e6253d3e703bef58f
Guilherme Parzianello <glhrm_@live.com>
2020-03-13 15:56:05 -0300

    one more missing migration

---
+++ migrations/20200313155132_remove_action_responsible_id.js
@@ -0,0 +1,11 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.alterTable('actions', table => {
+        table.dropColumn('agentId')
+    })
+}
+
+exports.down = function (knex, Promise) {
+    return knex.schema.alterTable('actions', table => {
+        table.integer('agentId').references('id').inTable('users').notNull()
+    }) 
+}
